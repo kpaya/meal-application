cd4eb36ee3092fbc3ce80bd1aa3a02c417f45398
kpaya <matteus.santos03@gmail.com>
2021-05-27 01:11:47 -0300

    Added - JQuery / Layout changed (Modify - HTML)

--- resources/js/utils.js
+++ resources/js/utils.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         id = $(this).attr('instructionId');
         $.get("https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + id, function(data, status) {
             if (status == 'success'){
-                responseFinal = JSON.stringify(data.meals[0].strInstructions);
+                responseFinal = data.meals[0].strInstructions;
                 $('#instructionField').html(responseFinal);
             }else{
                 $('#instructionField').html('Erro ao carregar! Tente novamente.');
